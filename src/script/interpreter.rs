65d4e31bea98ad0a5037c5208b15db414f22feb0
murphsicles <roycoins@gmail.com>
2025-06-12 11:13:44 +0100

    Update interpreter.rs

--- src/script/interpreter.rs
+++ src/script/interpreter.rs
@@ -8,7 +8,7 @@ use crate::util::{hash160, lshift, rshift, sha256d, Error, Result};
 use digest::{FixedOutput, Input};
 use num_bigint::BigInt;
 use num_traits::{One, ToPrimitive, Zero};
-use ring::digest::{digest, SHA1, SHA256};
+use ring::digest::{digest, SHA1_FOR_LEGACY_USE_ONLY, SHA256};
 use ripemd160::{Digest, Ripemd160};
 
 // Stack capacity defaults, which may exceeded
