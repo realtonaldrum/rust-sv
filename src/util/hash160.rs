b9d0fddc9c9981f90879f77c3d5d8639cb6874fd
Roy Murphy <roycoins@gmail.com>
2025-06-14 12:13:36 +0100

    Update hash160.rs

--- src/util/hash160.rs
+++ src/util/hash160.rs
@@ -3,17 +3,15 @@ use ring::digest::{digest, SHA256};
 use ripemd160::Ripemd160;
 use std::fmt;
 
-/// 160-bit hash for public key addresses
 #[derive(Default, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct Hash160(pub [u8; 20]);
 
-/// Hashes a data array once with SHA256 and again with RIPEMD160
 pub fn hash160(data: &[u8]) -> Hash160 {
     let sha256 = digest(&SHA256, data);
-    let mut ripemd160 = Ripemd160Digest::new();
-    ripemd160.update(sha256.as_ref()); // Changed from process
+    let mut ripemd160 = Ripemd160::new();
+    ripemd160.update(sha256.as_ref());
     let mut hash160 = [0; 20];
-    hash160.copy_from_slice(&ripemd160.finalize()); // Changed from fixed_result
+    hash160.copy_from_slice(&ripemd160.finalize());
     Hash160(hash160)
 }
 
