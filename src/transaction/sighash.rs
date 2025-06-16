f7addb0c59bba88e1fca8421ce1473bfb27afa05
Roy Murphy <roycoins@gmail.com>
2025-06-16 17:21:51 +0100

    Remove unused imports in sighash.rs

--- src/transaction/sighash.rs
+++ src/transaction/sighash.rs
@@ -1,8 +1,6 @@
 //! Transaction sighash helpers
 
-use crate::address::{addr_decode, AddressType};
 use crate::messages::{OutPoint, Payload, Tx, TxOut};
-use crate::network::NetworkConfig;
 use crate::script::{next_op, op_codes, Script};
 use crate::util::{sha256d, var_int, Error, Hash256, Result, Serializable};
 use byteorder::{LittleEndian, WriteBytesExt};
