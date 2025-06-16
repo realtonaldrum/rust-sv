5094905fa3820eb2ac10e2173eff9c2cc52b5cb5
Roy Murphy <roycoins@gmail.com>
2025-06-16 08:36:35 +0100

    Add address type crate

--- src/transaction/sighash.rs
+++ src/transaction/sighash.rs
@@ -1,7 +1,8 @@
 //! Transaction sighash helpers
 
+use crate::address::{addr_decode, AddressType};
 use crate::messages::{OutPoint, Payload, Tx, TxOut};
-use crate::network::NetworkConfig;
+use crate::network::NetworkConfig; // Keep
 use crate::script::{next_op, op_codes, Script};
 use crate::util::{sha256d, var_int, Error, Hash256, Result, Serializable};
 use byteorder::{LittleEndian, WriteBytesExt};
